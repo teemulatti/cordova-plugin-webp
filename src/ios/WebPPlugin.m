db3125a26d899d8652a4ff3562b4501c7bd8f2d0
Teemu Lätti <teemu.latti@wellevo.com>
2018-02-19 19:13:45 +0200

    c

--- src/ios/WebPPlugin.m
+++ src/ios/WebPPlugin.m
@@ -9,6 +9,11 @@
 
 - (void) decodeWebp:(CDVInvokedUrlCommand*)command
 {
+    //!
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"ok"];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    return;
+    
     if ([command.arguments count] == 0) {
         CDVPluginResult *pluginResult;
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No path provided"];
@@ -17,11 +22,6 @@
     }
     
     NSString * const webpPath = [command.arguments objectAtIndex:0];
-    
-    //!
-    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"ok"];
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-    
     //NSData * const webpData = [[NSData alloc] initWithContentsOfFile:webpPath options:NSDataReadingMappedIfSafe error:NULL];
     //UIImage * image = [STWebPDecoder imageWithData:webpData scale:1 error:NULL];
     //NSString * base64 = [NSString stringWithFormat:@"data:image/png;base64,%@",[UIImagePNGRepresentation(image) //base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength]];
